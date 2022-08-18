f830fe00b06382c1e615157023d51a81b86fc040
Subham Sahu <about.subhamsahu@gmail.com>
2022-08-18 20:21:44 +0530

    18th Aug C5

---
+++ test/test.js
@@ -0,0 +1,8 @@
+// DEEP COPY USING map() ARRAY METHOD
+
+const num = [1, 2, 3];
+
+const doubledNum = num.map((item) => item * 2);
+
+console.log(num); // [1, 2, 3]
+console.log(doubledNum); // [2, 4, 6]
